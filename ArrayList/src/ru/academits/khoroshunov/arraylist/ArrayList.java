4cd254f04a47d6f24f1a53ed953aee4356fea3fb
kh11884 <kh11884@ngs.ru>
2019-07-09 19:23:35 +0700

    Добавлена задача ArrayList.

---
+++ ArrayList/src/ru/academits/khoroshunov/arraylist/ArrayList.java
@@ -0,0 +1,329 @@
+package ru.academits.khoroshunov.arraylist;
+
+import java.util.*;
+
+public class ArrayList<E> implements List {
+    private int length;
+    private int modCount;
+    private E[] items;
+
+    public ArrayList() {
+        //noinspection unchecked
+        items = (E[]) new Object[10];
+        length = 0;
+    }
+
+    public ArrayList(int capacity) {
+        //noinspection unchecked
+        items = (E[]) new Object[capacity];
+        length = 0;
+    }
+
+    @Override
+    public int size() {
+        return length;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return length == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        for (int i = 0; i < length; i++) {
+            if (Objects.equals(o, items[i])) {
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private class MyListIterator implements Iterator<E> {
+        private int currentIndex = -1;
+        private int saveModCount = modCount;
+
+        public boolean hasNext() {
+            return currentIndex + 1 < length;
+        }
+
+        public E next() {
+            if (currentIndex >= length) {
+                throw new NoSuchElementException();
+            }
+            if (saveModCount != modCount) {
+                throw new ConcurrentModificationException();
+            }
+            ++currentIndex;
+            return items[currentIndex];
+        }
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new MyListIterator();
+    }
+
+    @Override
+    public E[] toArray() {
+        return Arrays.copyOf(items, length);
+    }
+
+    @Override
+    public boolean add(Object data) {
+        if (length == items.length) {
+            increaseCapacity();
+        }
+        //noinspection unchecked
+        items[length] = (E) data;
+        modCount++;
+        length++;
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        for (int i = 0; i < length; i++) {
+            if (Objects.equals(o, items[i])) {
+                remove(i);
+                modCount++;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        //noinspection unchecked
+        E[] addedArray = (E[]) c.toArray();
+        if (length + addedArray.length >= items.length) {
+            ensureCapacity(length + addedArray.length);
+        }
+        System.arraycopy(addedArray, 0, items, length, addedArray.length);
+        length += addedArray.length;
+        modCount++;
+        return true;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection c) {
+        //noinspection unchecked
+        E[] addedArray = (E[]) c.toArray();
+        int resultArrayLength = length + addedArray.length;
+        //noinspection unchecked
+        E[] resultArray = (E[]) new Object[resultArrayLength];
+        System.arraycopy(items, 0, resultArray, 0, index);
+        System.arraycopy(addedArray, 0, resultArray, index, addedArray.length);
+        System.arraycopy(items, index, resultArray, index+addedArray.length, length - index);
+        items = resultArray;
+        length = resultArrayLength;
+        modCount++;
+        return true;
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+        boolean isModified = false;
+        //noinspection unchecked
+        E[] newArray = (E[]) c.toArray();
+        for (int i = 0; i < length; i++) {
+            boolean mustBeDeleted = true;
+            for (E e : newArray) {
+                if (Objects.equals(items[i], e)) {
+                    mustBeDeleted = false;
+                    break;
+                }
+            }
+            if (mustBeDeleted) {
+                remove(i);
+                i--;
+                isModified = true;
+            }
+        }
+        return isModified;
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+        boolean isModified = false;
+        //noinspection unchecked
+        E[] newArray = (E[]) c.toArray();
+        for (E e : newArray) {
+            for (int j = 0; j < length; j++) {
+                if (Objects.equals(items[j], e)) {
+                    remove(j);
+                    j--;
+                    isModified = true;
+                }
+            }
+        }
+        return isModified;
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        boolean isContains;
+        //noinspection unchecked
+        E[] newArray = (E[]) c.toArray();
+        for (E e : newArray) {
+            isContains = false;
+            for (int i = 0; i < length; i++) {
+                if (Objects.equals(items[i], e)) {
+                    isContains = true;
+                    break;
+                }
+            }
+            if (!isContains) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        if(a.length >= length) {
+            a = Arrays.copyOf(toArray(), a.length);
+        } else{
+            a = toArray();
+        }
+        return a;
+    }
+
+    @Override
+    public E get(int index) {
+        if (index < 0 || index >= length) {
+            throw new IndexOutOfBoundsException("Значение индекса за пределами списка.");
+        }
+        return items[index];
+    }
+
+    @Override
+    public E set(int index, Object data) {
+        if (index < 0 || index >= length) {
+            throw new IndexOutOfBoundsException("Значение индекса за пределами списка.");
+        }
+        //noinspection unchecked
+        items[index] = (E) data;
+        return items[index];
+    }
+
+    @Override
+    public void add(int index, Object element) {
+        if (index < 0 || index > length) {
+            throw new IndexOutOfBoundsException("Значение индекса за пределами списка.");
+        }
+
+        if (length >= items.length) {
+            increaseCapacity();
+        }
+
+        System.arraycopy(items, index, items, index + 1, length - index);
+        //noinspection unchecked
+        items[index] = (E) element;
+        modCount++;
+        length++;
+    }
+
+    public E remove(int index) {
+        if (index < 0 || index >= length) {
+            throw new IndexOutOfBoundsException("Значение индекса за пределами списка.");
+        }
+        E result = items[index];
+        if (index < length - 1) {
+            System.arraycopy(items, index + 1, items, index, length - index - 1);
+        }
+        length--;
+        modCount++;
+        return result;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        for (int i = 0; i < length; i++) {
+            if (Objects.equals(o, items[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        int result = -1;
+        for (int i = 0; i < length; i++) {
+            if (Objects.equals(o, items[i])) {
+                result = i;
+            }
+        }
+        return result;
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Override
+    public ListIterator listIterator() {
+        return null;
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Override
+    public ListIterator listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public ArrayList subList(int fromIndex, int toIndex) {
+        if (fromIndex < 0 || fromIndex >= length || toIndex < 0 || toIndex >= length) {
+            throw new IndexOutOfBoundsException("Значение индекса за пределами списка.");
+        }
+        if (fromIndex > toIndex) {
+            throw new IllegalArgumentException("Неправильные значения индекса");
+        }
+
+        int resultLength = toIndex - fromIndex;
+        ArrayList result = new ArrayList(resultLength);
+        result.length = resultLength;
+        System.arraycopy(items, fromIndex, result.items, 0,resultLength);
+        return result;
+    }
+
+    private void ensureCapacity(int requiredCapacity) {
+        if (requiredCapacity > items.length) {
+            int newSize = items.length;
+            while (newSize < requiredCapacity) {
+                newSize *= 2;
+            }
+            items = Arrays.copyOf(items, newSize);
+        }
+    }
+
+    private void increaseCapacity() {
+        items = Arrays.copyOf(items, items.length * 2);
+    }
+
+
+    public void trimToSize() {
+        items = Arrays.copyOf(items, length);
+    }
+
+    public void clear() {
+        modCount++;
+        length = 0;
+    }
+
+    public String toString() {
+        StringBuilder b = new StringBuilder();
+        b.append('[');
+        for (int i = 0; i < length; i++) {
+            b.append(items[i]);
+            if (i < length - 1) {
+                b.append(", ");
+            }
+        }
+        return b.append(']').toString();
+    }
+}
+
+
---
+++ ArrayList/src/ru/academits/khoroshunov/main/Main.java
@@ -0,0 +1,192 @@
+package ru.academits.khoroshunov.main;
+
+import ru.academits.khoroshunov.arraylist.ArrayList;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Проверка создания списка на массиве. Конструктор: ArrayList()");
+        ArrayList<String> arrayList1 = new ArrayList<>();
+        System.out.println(arrayList1);
+        System.out.println();
+
+        System.out.println("Проверка создания списка на массиве. Конструктор: ArrayList(int capacity)");
+        ArrayList arrayList2 = new ArrayList(12);
+        System.out.println(arrayList2);
+        System.out.println();
+
+        arrayList1.add("a");
+        arrayList1.add("b");
+        arrayList1.add("b");
+        arrayList1.add("c");
+        arrayList1.add("d");
+        arrayList1.add("e");
+        arrayList1.add("f");
+        System.out.println("Исходный список на массиве");
+        System.out.println(arrayList1 + " Длина списка на массиве: " + arrayList1.size());
+        System.out.println();
+
+        System.out.println("Проверка метора contains(Object o)");
+        String value1 = "b";
+        String value2 = "h";
+        System.out.println("Результат работы метода, проверяем есть ли \"" + value1 + "\" - " + arrayList1.contains(value1));
+        System.out.println("Результат работы метода, проверяем есть ли \"" + value2 + "\" - " + arrayList1.contains(value2));
+        System.out.println();
+
+        System.out.println("Проверка итератора. Выводим содержимое списка:");
+        Iterator i = arrayList1.iterator();
+        for (; i.hasNext();) {
+            String text = String.valueOf(i.next());
+            System.out.print(text + " ");
+        }
+        System.out.println();
+        System.out.println();
+
+        System.out.println("Проверка метода E[] toArray()");
+        System.out.println(Arrays.toString(arrayList1.toArray()));
+        System.out.println();
+
+        int index1 = 0;
+        System.out.println("Проверка метора remove. Удалим элемент индекса: " + index1);
+        arrayList1.remove(index1);
+        System.out.println(arrayList1 + " Длина списка: " + arrayList1.size());
+        System.out.println();
+
+        List collection1 = Arrays.asList("b", "d");
+        System.out.println("Проверка метора addAll. Добавим коллекцию: " + collection1.toString());
+        arrayList1.addAll(collection1);
+        System.out.println(arrayList1 + " Длина списка: " + arrayList1.size());
+        System.out.println();
+
+        List collection2 = Arrays.asList("x", "y");
+        int index2 = 0;
+        System.out.println("Проверка метора addAll(int index, Collection c). Добавим коллекцию: " + collection2.toString() + " по индексу " + index2);
+        System.out.println("до операции");
+        System.out.println(arrayList1 + " Длина списка: " + arrayList1.size());
+        arrayList1.addAll(index2, collection2);
+        System.out.println("после операции");
+        System.out.println(arrayList1 + " Длина списка: " + arrayList1.size());
+        System.out.println();
+
+        List collection3 = Arrays.asList("x", "y", "b");
+        System.out.println("Проверка метода retainAll. Удаляем элементы не принадлежащие коллекции " + collection3.toString());
+        System.out.println("до операции");
+        System.out.println(arrayList1 + " Длина списка: " + arrayList1.size());
+        arrayList1.retainAll(collection3);
+        System.out.println("после операции");
+        System.out.println(arrayList1 + " Длина списка: " + arrayList1.size());
+        System.out.println();
+
+        arrayList1.add(0, "a");
+        arrayList1.add(3, "c");
+        arrayList1.add(5, "e");
+        arrayList1.add(6, "f");
+        List collection4 = Arrays.asList("x", "b");
+        System.out.println("Проверка метода removeAll. Удаляем элементы принадлежащие коллекции " + collection4.toString());
+        System.out.println("до операции");
+        System.out.println(arrayList1 + " Длина списка: " + arrayList1.size());
+        arrayList1.removeAll(collection4);
+        System.out.println("после операции");
+        System.out.println(arrayList1 + " Длина списка: " + arrayList1.size());
+        System.out.println();
+
+        arrayList1.add(1, "b");
+        arrayList1.add(2, "b");
+        arrayList1.add(4, "d");
+        List collection5 = Arrays.asList("f", "b");
+        System.out.println("Проверка метода containAll. Проверяем есть ли все элементы принадлежащие коллекции " + collection5.toString());
+        System.out.println("до операции");
+        System.out.println(arrayList1 + " Длина списка: " + arrayList1.size());
+        System.out.println(arrayList1.containsAll(collection5));
+        System.out.println("после операции");
+        System.out.println(arrayList1 + " Длина списка: " + arrayList1.size());
+        System.out.println();
+
+        System.out.println("Проверка метода Object[] toArray(Object[] a)");
+        System.out.println("до операции");
+        System.out.println(arrayList1 + " Длина списка: " + arrayList1.size());
+        Object[] array1 = new String[15];
+        array1 = arrayList1.toArray(array1);
+        System.out.println("Получившийся массив ");
+        System.out.println(Arrays.toString(array1));
+        System.out.println();
+
+        int index3 = 7;
+        System.out.println("Проверка метода get");
+        System.out.println("до операции");
+        System.out.println(arrayList1 + " Длина списка: " + arrayList1.size());
+        System.out.println("Элемент по индексу " + index3 + " - " + arrayList1.get(index3));
+        System.out.println();
+
+        int index4 = 0;
+        String element1 = "e";
+        System.out.println("Проверка метода set");
+        System.out.println("до операции");
+        System.out.println(arrayList1 + " Длина списка: " + arrayList1.size());
+        System.out.println("Вставляем элемент \"" + element1 + "\" по индексу - " + index4);
+        arrayList1.set(index4, element1);
+        System.out.println("после операции");
+        System.out.println(arrayList1 + " Длина списка: " + arrayList1.size());
+        System.out.println();
+
+        int index5 = 7;
+        System.out.println("Проверка метода remove");
+        System.out.println("до операции");
+        System.out.println(arrayList1 + " Длина списка: " + arrayList1.size());
+        System.out.println("Удаляем элемент по индексу - " + index5);
+        String element2 = arrayList1.remove(index5);
+        System.out.println("после операции");
+        System.out.println(arrayList1 + " Длина списка: " + arrayList1.size());
+        System.out.println("Удаленный элемент: " + element2);
+        System.out.println();
+
+        String element3 = "e";
+        System.out.println("Проверка метода indexOf");
+        System.out.println("до операции");
+        System.out.println(arrayList1 + " Длина списка: " + arrayList1.size());
+        System.out.println("Элемент \"" + element3 + "\" находится по индексу: " + arrayList1.indexOf(element3));
+        System.out.println("после операции");
+        System.out.println(arrayList1 + " Длина списка: " + arrayList1.size());
+        System.out.println();
+
+        String element4 = "e";
+        System.out.println("Проверка метода LastIndexOf");
+        System.out.println("до операции");
+        System.out.println(arrayList1 + " Длина списка: " + arrayList1.size());
+        System.out.println("Элемент \"" + element4 + "\" последний индекс вхождения: " + arrayList1.lastIndexOf(element4));
+        System.out.println("после операции");
+        System.out.println(arrayList1 + " Длина списка: " + arrayList1.size());
+        System.out.println();
+
+        int index6 = 1;
+        int index7 = 3;
+        System.out.println("Проверка метода subList");
+        System.out.println("до операции");
+        System.out.println(arrayList1 + " Длина списка: " + arrayList1.size());
+        ArrayList subList = arrayList1.subList(index6, index7);
+        System.out.println("после операции. Исходный список");
+        System.out.println(arrayList1 + " Длина списка: " + arrayList1.size());
+        System.out.println("после операции. Возвращенный подсписок от индекса " + index6 + " до индекса " + index7);
+        System.out.println(subList + " Длина подсписка: " + subList.size());
+        System.out.println();
+
+        System.out.println("Проверка метода trimToSize");
+        System.out.println("до операции");
+        System.out.println(arrayList1 + " Длина списка: " + arrayList1.size());
+        arrayList1.trimToSize();
+        System.out.println("после операции.");
+        System.out.println(arrayList1 + " Длина списка: " + arrayList1.size());
+        System.out.println();
+
+        System.out.println("Проверка метода clear");
+        System.out.println("до операции");
+        System.out.println(arrayList1 + " Длина списка: " + arrayList1.size());
+        arrayList1.clear();
+        System.out.println("после операции.");
+        System.out.println(arrayList1 + " Длина списка: " + arrayList1.size());
+        System.out.println();
+    }
+}
