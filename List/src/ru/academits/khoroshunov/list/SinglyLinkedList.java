42fc77bce61f00a4aa77e3771f42df2b285f72f7
kh11884 <kh11884@ngs.ru>
2019-07-09 16:07:46 +0700

    Доработан метод copy задачи List.

--- List/src/ru/academits/khoroshunov/list/SinglyLinkedList.java
+++ List/src/ru/academits/khoroshunov/list/SinglyLinkedList.java
@@ -168,10 +168,9 @@ public class SinglyLinkedList<T> {
         }
 
         newList.head = new ListItem<>(head.getData());
-        newList.count++;
+        newList.count = count;
         for (ListItem<T> p = head.getNext(), newP = newList.head; p != null; p = p.getNext(), newP = newP.getNext()) {
             newP.setNext(new ListItem<>(p.getData()));
-            newList.count++;
         }
         return newList;
     }
